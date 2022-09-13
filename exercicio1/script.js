bf5fafa0b5f24b305513ab3e58d2c584ac4b37f4
Casenrique <carloshenriquesouza.eng@gmail.com>
2022-09-13 16:03:41 -0300

    Adicionando os exercícios após a aula de feedback

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,7 +1,19 @@
 const array = [
-    [27, 4, 20, 13, 14],
-    [11, 15, 12, 8, 9],
-    [5, 5, 12, 16, 4],
-    [20, 33, 11, 12, 19],
-    [3, 3, 4, 5, 10]
+    [27, 4, 20, 13, 14],  //Jogador 1
+    [11, 15, 12, 8, 9],   //Jogador 2
+    [5, 5, 12, 16, 4],    //Jogador 3
+    [20, 33, 11, 12, 19], //Jogador 4
+    [3, 3, 4, 5, 10]      //Jogador 5
 ]
+
+// for(let i in array){
+//     console.log(`Jogador ${Number(i)+1}: ${array[i]} `)
+// }
+
+for(let i in array){
+    let jogador = `Jogador ${Number(i)+1}: `
+    for(let j of array[i]){
+        jogador += j + ", "
+    }
+    console.log(jogador)
+}
